841a5f1bb8dd82616bb00165c43a04a4a69fa5a0
Marion Corvez <marion.corvez@gmail.com>
2024-01-18 02:22:09 +0100

    Étape 2 : Ajouter des Event Listeners sur les flèches

--- ReadMe.md
+++ ReadMe.md
@@ -3,7 +3,7 @@
 ## Étapes
 
 1. [X] Étape 1 : Mettre à jour le code HTML
-2. [ ] Étape 2 : Ajouter des Event Listeners sur les flèches
+2. [X] Étape 2 : Ajouter des Event Listeners sur les flèches
 3. [ ] Étape 3 : Ajouter des bullet points au slider
 4. [ ] Étape 4 : Modifier le slide au clic sur le bouton
 5. [ ] Étape 5 : Mettre en place le défilement infini sur le carrousel
--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,49 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+/// Étape 2 : Ajouter des Event Listeners sur les flèches
+
+// Mettre un event listener sur chacune des flèches
+// Ajouter un console.log ou une alert() pour tester le fonctionnement des event listeners
+// S'assurer qu'on peut différencier le clic sur le bouton gauche du clic sur le bouton droit. 
+
+// Solution 1
+/*let arrowLeft = document.querySelector(".arrow_left");
+arrowLeft.addEventListener("click", () => {
+  console.log("Vous avez cliqué sur la flèche de gauche")
+});
+
+let arrowRight = document.querySelector(".arrow_right");
+arrowRight.addEventListener("click", () => {
+  console.log("Vous avez cliqué sur la flèche de droite")
+});*/
+
+
+// Solution 2 : ajout d'un ID
+/*let listArrows = document.querySelectorAll(".arrow");
+
+for (let i = 0; i < listArrows.length; i++) {
+	let currentArrow = listArrows[i]
+	console.log(currentArrow)
+
+	currentArrow.addEventListener("click", (event) => {
+		myArrow = event.target.className
+		console.log(`J'ai cliqué sur la flèche : ${myArrow.id}`);
+	});
+	
+}*/
+
+// Solution 3 : affichage des classes
+let listArrows = document.querySelectorAll(".arrow");
+
+for (let i = 0; i < listArrows.length; i++) {
+	let currentArrow = listArrows[i]
+	console.log(currentArrow)
+
+	currentArrow.addEventListener("click", (event) => {
+		myArrow = event.target.className
+    console.log(`J'ai cliqué sur la flèche : ${myArrow}`);
+	});
+	
+}
